d3bb4c8209e83fdc98f166b08b421a3c0dcd0d79
Caleb Knight <knightca@oregonstate.edu>
2022-05-15 13:07:01 -0700

    Add test to check for negative demand

--- test/province_test.rb
+++ test/province_test.rb
@@ -39,5 +39,12 @@ class ProvinceTest < Minitest::Test
   def test_province_zero_demand
     asia.demand = 0
     assert_equal(-25, asia.shortfall)
+    assert_equal(0, asia.profit)
+  end
+
+  def test_province_negative_demand
+    asia.demand = -1
+    assert_equal(-26, asia.shortfall)
+    assert_equal(-10, asia.profit)
   end
 end
